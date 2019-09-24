7dd37e1f34861a846b7162fe9a904574cb76964f
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2019-09-24 11:21:16 +0530

    baseurl changes

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: "Coder Wiki", // Title for your website.
   tagline: 'Knowledge gathered during my technical learning',
   url: 'https://coder-selvarajan.github.io', // Your website URL
-  baseUrl: '/mywiki/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
